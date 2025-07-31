7f3b724396affebfb81738cb8d0362ae5b586d13
CMKuDu <nguyentrandau2018@gmail.com>
2025-07-31 17:22:44 +0700

    msg

--- src/app/components/Languages.tsx
+++ src/app/components/Languages.tsx
@@ -1,6 +1,6 @@
-"use client";
+"use client"
 
-import * as React from "react";
+import * as React from "react"
 import {
   DropdownMenu,
   DropdownMenuCheckboxItem,
@@ -8,53 +8,40 @@ import {
   DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { Button } from "@/components/ui/button";
-import { Languages } from "lucide-react";
-import { useLocale, useTranslations } from "next-intl";
-import { usePathname, useRouter } from "@/i18n/navigation";
-import { useMemo } from "react";
+} from "@/components/ui/dropdown-menu"
+import { Button } from "@/components/ui/button"
+import { Languages } from "lucide-react"
+import { useTranslations } from "next-intl"
+import { usePathname, useRouter } from "@/i18n/navigation"
+
 export function LanguageSwitcher() {
   const t = useTranslations();
   const router = useRouter();
   const pathname = usePathname();
-  const currentLocale = useLocale();
 
   const locales = [
     { code: "vn", label: t("Header.Languages.VietNam") },
-    { code: "en", label: t("Header.Languages.English") },
+    { code: "en", label: t("Header.Languages.English") }
   ];
-  const basePath = useMemo(() => {
-    const parts = pathname.split("/");
-    parts[1] = ""; // xoá locale hiện tại
-    return parts.join("/") || "/";
-  }, [pathname]);
+
   const handleChangeLocale = (locale: string) => {
-    router.replace(basePath, { locale });
+    window.location.href = `${pathname}${locale}`;
   };
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button
-          variant="ghost"
-          size="icon"
-          title={t("Header.Languages.Languages")}
-          className="hover:text-blue-500"
-        >
+        <Button variant="ghost" size="icon" title={t("Header.Languages.Languages")} className="hover:text-blue-500">
           <Languages size={24} />
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-56">
-        <DropdownMenuLabel>
-          {t("Header.Languages.Languages")}
-        </DropdownMenuLabel>
+        <DropdownMenuLabel>{t('Header.Languages.Languages')}</DropdownMenuLabel>
         <DropdownMenuSeparator />
         {locales.map((lang) => (
           <DropdownMenuCheckboxItem
             key={lang.code}
-            checked={lang.code === currentLocale}
-            onCheckedChange={() => handleChangeLocale(lang.code)}
+            onClick={() => handleChangeLocale(lang.code)}
             className="font-bold"
           >
             {lang.label}
